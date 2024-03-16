571549a5785ff774e493e4532ce00b32de76c644
atik555555 <atikabdullah555555@gmail.com>
2024-03-16 16:29:41 +0600

    bangladesh

--- db/arma.sql
+++ db/arma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2024 at 11:14 AM
+-- Generation Time: Mar 16, 2024 at 11:27 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -20,54 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `arma`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -94,29 +46,13 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 (1, 'Rahim uddin', 'admin@gmail.com', NULL, '$2y$10$mSOSucNfiaHihAzzz8Ud3OTlZMRDw37grHlKahB4j9qcfFTiS1uzK', NULL, '2024-03-16 03:09:10', '2024-03-16 03:09:10'),
 (2, 'atik', 'atik@gmail.com', NULL, '$2y$10$9ktl0u.jrJmj0MDiWmPNJuVpUMq0S3qre3ekpM1QvFJVbQ5xIy06q', NULL, '2024-03-16 03:24:01', '2024-03-16 03:24:01'),
 (3, 'SHANJOY DAS SHANJIT', 'shanjit@gmail.com', NULL, '$2y$10$838Xbk2Dqi6/tdy.scv5p.D7shpYhBeEWXxgxWeiEyYnWaVmkg3le', NULL, '2024-03-16 03:43:12', '2024-03-16 03:43:12'),
-(4, 'tawfiq', 'tawfiq@gmail.com', NULL, '$2y$10$EL4WIn7hp97SUrRJ8XQekOcE7cIK2McZCBOhiHtGv27cUKo4qCNH6', NULL, '2024-03-16 04:14:07', '2024-03-16 04:14:07');
+(4, 'tawfiq', 'tawfiq@gmail.com', NULL, '$2y$10$EL4WIn7hp97SUrRJ8XQekOcE7cIK2McZCBOhiHtGv27cUKo4qCNH6', NULL, '2024-03-16 04:14:07', '2024-03-16 04:14:07'),
+(5, 'tamim', 'tamim@gmail.com', NULL, '$2y$10$90xMdEw4aAyNoP9NbiZqK.9NplLsoBaI0oYpGbyhgG6nfVrjUJbxK', NULL, '2024-03-16 04:24:54', '2024-03-16 04:24:54'),
+(6, 'sakib', 'sakib@gmail.com', NULL, '$2y$10$MD3n1mFZGHre9VSnWl3ZQOeqaqMB9dsyCNSley57VVyHkVqCBvrqm', NULL, '2024-03-16 04:25:40', '2024-03-16 04:25:40');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
 
 --
 -- Indexes for table `users`
@@ -130,22 +66,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
