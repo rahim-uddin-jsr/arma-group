c5ad59635cc0e687a28a2b7b969471c784f7c058
Rahim Uddin <rahimuddin5915@gmail.com>
2024-03-18 15:06:42 +0600

    contact and career page added done

---
+++ main/resources/views/frontend/inc/scripts.blade.php
@@ -0,0 +1,27 @@
+<script src="{{ asset('assets/frontend/js/vendor/jquery-1.12.0.min.js') }}"></script>
+
+<!-- bootstrap JS -->
+<script src="{{ asset('assets/frontend/js/bootstrap.min.js') }}"></script>
+<!-- owl.carousel JS -->
+<script src="{{ asset('assets/frontend/js/owl.carousel.min.js') }}"></script>
+<!-- bootstrap select JS -->
+<script src="{{ asset('assets/frontend/js/bootstrap-select.min.js') }}"></script>
+<!-- magnific popup JS -->
+<script src="{{ asset('assets/frontend/js/jquery.magnific-popup.min.js') }}"></script>
+<!-- jquery-ui JS -->
+<script src="{{ asset('assets/frontend/js/jquery-ui.min.js') }}"></script>
+<!-- wow JS -->
+<script src="{{ asset('assets/frontend/js/wow.min.js') }}"></script>
+<!-- bxslider JS -->
+<script src="{{ asset('assets/frontend/js/jquery.bxslider.min.js') }}"></script>
+<!-- counterup JS -->
+<script src="{{ asset('assets/frontend/js/jquery.counterup.min.js') }}"></script>
+<script src="{{ asset('assets/frontend/js/waypoints.min.js') }}"></script>
+<!-- slick JS -->
+<script src="{{ asset('assets/frontend/js/slick.min.js') }}"></script>
+<!-- meanmenu js -->
+<script src="{{ asset('assets/frontend/js/jquery.meanmenu.js') }}"></script>
+<!-- plugins JS -->
+<script src="{{ asset('assets/frontend/js/plugins.js') }}"></script>
+<!-- main JS -->
+<script src="{{ asset('assets/frontend/js/main.js') }}"></script>
