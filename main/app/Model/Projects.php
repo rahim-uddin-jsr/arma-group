a5ab92157576c2dff03be501001655b2e03ec689
Rahim Uddin <rahimuddin5915@gmail.com>
2024-03-19 11:50:28 +0600

    key feature table created and relationship with single project done

--- main/app/Model/Projects.php
+++ main/app/Model/Projects.php
@@ -11,4 +11,7 @@ class Projects extends Model
     public function images() {
         return $this->hasMany(ProjectImage::class,'project_id','id');
     }
+    public function keyFeature() {
+        return $this->hasMany(KeyFeature::class,'project_id','id');
+    }
 }
