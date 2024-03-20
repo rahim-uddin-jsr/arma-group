f1058af30ce25b9dca7bf02d478376118ea9d359
atik555555 <atikabdullah555555@gmail.com>
2024-03-20 15:39:16 +0600

    sorted script file in header & introduced push

--- main/resources/views/backend/inc/footer.blade.php
+++ main/resources/views/backend/inc/footer.blade.php
@@ -4,13 +4,15 @@
 </div>
 <script src="{{asset('assets/backend/assets/vendor/js/jquery-3.6.0.min.js')}}"></script>
 <script src="{{asset('assets/backend/assets/vendor/js/jquery.overlayScrollbars.min.js')}}"></script>
-<script src="{{asset('assets/backend/assets/vendor/js/apexcharts.js')}}"></script>
-<script src="{{asset('assets/backend/assets/vendor/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('assets/backend/assets/vendor/js/moment.min.js')}}"></script>
-<script src="{{asset('assets/backend/assets/vendor/js/daterangepicker.js')}}"></script>
 <script src="{{asset('assets/backend/assets/vendor/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/backend/assets/js/dashboard.js')}}"></script>
 <script src="{{asset('assets/backend/assets/js/main.js')}}"></script>
+
+
+
+<!-- <script src="assets/vendor/js/jquery-3.6.0.min.js"></script> -->
+    <!-- <script src="assets/vendor/js/jquery.overlayScrollbars.min.js"></script> -->
+    <!-- <script src="assets/vendor/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/main.js"></script> -->
 <!-- for demo purpose -->
 <script>
     var rtlReady = $('html').attr('dir', 'ltr');
