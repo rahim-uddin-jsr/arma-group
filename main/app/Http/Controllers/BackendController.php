db887259ccff3f2f46e94928dce7900fc484a754
Rahim Uddin <rahimuddin5915@gmail.com>
2024-03-23 15:57:42 +0600

    project update feature almost done

--- main/app/Http/Controllers/BackendController.php
+++ main/app/Http/Controllers/BackendController.php
@@ -18,74 +18,137 @@ class BackendController extends Controller
         return $projects;
         // return view('frontend.pages.contact');
     }
-     // projects section start
-     public function addProjectIndex()
-     {
-         // $data =Basicinfo::all();
-         return view('backend.pages.add-project');
-     }
-     public function addProject(Request $request)
-     {
-         $project_Id = Projects::insertGetId([
-             'first_name' => $request->first_name,
-             'last_name' => $request->last_name,
-             'location' => $request->address,
-             'status' => $request->status,
+    // projects section start
+    public function addProjectIndex()
+    {
+        // $data =Basicinfo::all();
+        return view('backend.pages.add-project', ['edit' => false]);
+    }
+    public function addProject(Request $request)
+    {
+        $project_Id = Projects::insertGetId([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'location' => $request->address,
+            'status' => $request->status,
 
-         ]);
-         $features = request('feature');
-         // dd(request('feature'));
-         foreach ($features as $key => $value) {
-             ProjectFeature::create([
-                 'special_feature' => $value,
-                 'project_id' => $project_Id,
-             ]);
-         }
-         $keyInfo = request('key_info');
-         // dd(request('feature'));
-         foreach ($keyInfo as $key => $value) {
-             KeyFeature::create([
-                 'key_info' => $value,
-                 'project_id' => $project_Id,
-             ]);
-         }
+        ]);
+        $features = request('feature');
+        // dd(request('feature'));
+        foreach ($features as $key => $value) {
+            ProjectFeature::create([
+                'special_feature' => $value,
+                'project_id' => $project_Id,
+            ]);
+        }
+        $keyInfo = request('key_info');
+        // dd(request('feature'));
+        foreach ($keyInfo as $key => $value) {
+            KeyFeature::create([
+                'key_info' => $value,
+                'project_id' => $project_Id,
+            ]);
+        }
 
-         if ($request->hasFile('project_images')) {
-             $files = $request->file('project_images');
-             foreach ($files as $key => $file) {
-                 // $extension = $file->getClientOriginalExtension();
-                 // $filename = 'image_' . time() . '.' . $extension;
-                 // $filename = $file->store('photos');
-                 $extension = $file->getClientOriginalExtension();
-                 // $name = $file->getClientOriginalName();
-                 $filename = uniqid() . Date('His') . '.' . $extension;
-                 $file->move('assets/uploads/projects-images', $filename);
-                 ProjectImage::create([
-                     'image' => 'assets/uploads/projects-images/' . $filename,
-                     'position' => $key,
-                     'project_id' => $project_Id,
-                 ]);
-             }
-         }
-         return back()->with('status', 'Successfully added!');
-         // $data =Basicinfo::all();
-         // return view('backend.pages.add-project');
-     }
+        if ($request->hasFile('project_images')) {
+            $files = $request->file('project_images');
+            foreach ($files as $key => $file) {
+                // $extension = $file->getClientOriginalExtension();
+                // $filename = 'image_' . time() . '.' . $extension;
+                // $filename = $file->store('photos');
+                $extension = $file->getClientOriginalExtension();
+                // $name = $file->getClientOriginalName();
+                $filename = uniqid() . Date('His') . '.' . $extension;
+                $file->move('assets/uploads/projects-images', $filename);
+                ProjectImage::create([
+                    'image' => 'assets/uploads/projects-images/' . $filename,
+                    'position' => $key,
+                    'project_id' => $project_Id,
+                ]);
+            }
+        }
+        return back()->with('status', 'Successfully added!');
+        // $data =Basicinfo::all();
+        // return view('backend.pages.add-project');
+    }
+    public function editProjectIndex($id)
+    {
+        $project = Projects::with('images', 'keyFeature', 'feature')->find($id);
+        return view('backend.pages.add-project', ['edit' => true], compact('project'));
+    }
+    public function deleteProjectImage($id)
+    {
+        $project = ProjectImage::find($id);
+        $isDelete=$project->delete();
+        if ($isDelete) {
+            return back()->with('status', 'Deleted Successfully!');
+        }
+        return ('something went wrong');
+    }
+    public function editProject(Request $request, $id)
+    {
+        $project = Projects::find($id);
 
+        ProjectFeature::where('project_id', '=', $id)->delete();
+        KeyFeature::where('project_id', '=', $id)->delete();
+        $features = request('feature');
+        // dd(request('feature'));
+        foreach ($features as $key => $value) {
+            if (!$value) {
+                continue;
+            }
+            ProjectFeature::create([
+                'special_feature' => $value,
+                'project_id' => $id,
+            ]);
+        }
+        $keyInfo = request('key_info');
+        foreach ($keyInfo as $key => $value) {
+            if (!$value) {
+                continue;
+            }
+            KeyFeature::create([
+                'key_info' => $value,
+                'project_id' => $id,
+            ]);
+        }
+        if ($request->hasFile('project_images')) {
+            $files = $request->file('project_images');
+            foreach ($files as $key => $file) {
+                $extension = $file->getClientOriginalExtension();
+                $filename = uniqid() . Date('His') . '.' . $extension;
+                $file->move('assets/uploads/projects-images', $filename);
+                ProjectImage::create([
+                    'image' => 'assets/uploads/projects-images/' . $filename,
+                    'position' => $key,
+                    'project_id' => $id,
+                ]);
+            }
+        }
+
+        $project->update([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'location' => $request->address,
+            'status' => $request->status,
+        ]);
+        // dd($request->all());
+        return back()->with('status', 'Successfully Updated!');
+    }
     public function upcomingProjects()
     {
-        $projects = Projects::withCount('keyFeature', 'feature')->with('images')->get()->whereIn('status','upcoming');
-        return view('backend.pages.upcoming-projects',compact('projects'));
+        $projects = Projects::withCount('keyFeature', 'feature')->with('images')->get()->whereIn('status', 'upcoming');
+        return view('backend.pages.upcoming-projects', compact('projects'));
     }
     public function ongoingProjects()
     {
-        $projects = Projects::withCount('keyFeature', 'feature')->with('images')->get()->whereIn('status','ongoing');
-        return view('backend.pages.ongoing-projects',compact('projects'));
+        $projects = Projects::withCount('keyFeature', 'feature')->with('images')->get()->whereIn('status', 'ongoing');
+        return view('backend.pages.ongoing-projects', compact('projects'));
     }
     public function completedProjects()
     {
-        $projects = Projects::withCount('keyFeature', 'feature')->with('images')->get()->whereIn('status','completed');
-        return view('backend.pages.completed-projects',compact('projects'));
+        $projects = Projects::withCount('keyFeature', 'feature')->with('images')->get()->whereIn('status', 'completed');
+        return view('backend.pages.completed-projects', compact('projects'));
     }
     public function deleteProject($id)
     {
